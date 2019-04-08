dd83ac678589856677142f75e79d4fa9b2c1a107
photos514 <46639346+photos514@users.noreply.github.com>
2019-04-08 18:49:05 -0400

    Update Startup.cs

--- API_Usage/Startup.cs
+++ API_Usage/Startup.cs
@@ -21,8 +21,8 @@ namespace API_Usage
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    // This method gets called by the runtime. This method can be used to add services to the container.
+	// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
       // Setup EF connection
